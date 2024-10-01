a80a808cd1d4b7faf3598608587f12467f904371
DESKTOP-5CTFEFT\mateusz <matimichalski01@gmail.com>
2024-10-01 11:20:14 +0200

    funkcja handle submit i tworzenie zmiennej

--- projekt-react/src/App.js
+++ projekt-react/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import React, { useState } from 'react';
 
 function App() {
+
+const[tytul,setTytul] = useState("");  
+const[rodzaj,setRodzaj] = useState("");
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+}
+
+console.log(`Tytuł:${tytul},Rodzaj:${rodzaj}`);
   return (
-    <div className="App">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <div className="container" style={{padding:"20px"}}>
+      <h1>Dodaj film</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="from-group">
+          <label htmlFor='tytuł'>Tytuł filmu</label>
+          <input type="text" id="tytul" className="from-control" value={tytul} onChange={(e) =>setRodzaj(e.target.value)}/>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+        <div className="form-group">
+          
         </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+        
       </form>
     </div>
   );
