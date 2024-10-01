e0ce0debb5ed84b659c2f1f042ebb8468fb9ae29
DESKTOP-5CTFEFT\mateusz <matimichalski01@gmail.com>
2024-10-01 11:22:55 +0200

    zakonczenie aplikacji

--- projekt-react/src/App.js
+++ projekt-react/src/App.js
@@ -1,31 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css'
 import React, { useState } from 'react';
-
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
-
-const[tytul,setTytul] = useState("");  
-const[rodzaj,setRodzaj] = useState("");
-
-const handleSubmit = (e) =>{
-  e.preventDefault();
-}
-
-console.log(`Tytuł:${tytul},Rodzaj:${rodzaj}`);
+  const [tytul, setTytul] = useState("");
+  const [rodzaj, setRodzaj] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Tutył: ${tytul}, Rodzaj: ${rodzaj}`);
+  };
   return (
-    <div className="container" style={{padding:"20px"}}>
+    <div className="container" style={{ padding: "20px" }}>
       <h1>Dodaj film</h1>
       <form onSubmit={handleSubmit}>
-        <div className="from-group">
-          <label htmlFor='tytuł'>Tytuł filmu</label>
-          <input type="text" id="tytul" className="from-control" value={tytul} onChange={(e) =>setRodzaj(e.target.value)}/>
-        </div>
         <div className="form-group">
-          
+          <label htmlFor='tytuł'>Tytuł filmu:</label>
+          <input 
+          type='text'
+          id='tytul'
+          className='form-control'
+          value={tytul}
+          onChange={(e)=>setTytul(e.target.value)}
+          />
         </div>
-
-        
+        <div className='form-group'>
+          <label htmlFor='rodzaj'>Rodzaj filmu:</label>
+          <select
+          id='rodzaj'
+          className='form-control'
+          value={rodzaj}
+          onChange={(e)=>setRodzaj(e.target.value)}
+          >
+            <option id="">1</option>
+            <option id="1">2</option>
+            <option id="2">3</option>
+            <option id="3">4</option>
+            <option id="4">5</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">Dodaj</button>
       </form>
     </div>
   );
